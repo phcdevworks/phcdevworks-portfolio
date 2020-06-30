d63136bfc1ee3ef9cefddf186a91d7c8d0470a9f
BradPotts <brad.potts@phcnet.co>
2020-06-30 11:14:50 +0000

    updated test dbs and file checks.

--- test/test_app/db/migrate/20200630111348_create_phcdevworks_portfolio_posts_types.phcdevworks_portfolio.rb
+++ test/test_app/db/migrate/20200630111348_create_phcdevworks_portfolio_posts_types.phcdevworks_portfolio.rb
@@ -2,8 +2,10 @@
 class CreatePhcdevworksPortfolioPostsTypes < ActiveRecord::Migration[6.0]
   def change
     create_table :phcdevworks_portfolio_posts_types do |t|
+
       t.integer :post_id
       t.integer :type_id
+
     end
   end
 end
