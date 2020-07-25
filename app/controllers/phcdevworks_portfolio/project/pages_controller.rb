882aacd50607a7913652dd2f0a7ba23a58f0c61d
BradPotts <brad.potts@phcnet.co>
2020-07-25 10:34:17 +0000

    fixes to frontend controller.

--- app/controllers/phcdevworks_portfolio/project/pages_controller.rb
+++ app/controllers/phcdevworks_portfolio/project/pages_controller.rb
@@ -8,7 +8,7 @@ module PhcdevworksPortfolio
 
     # INDEX
     def index
-      @phcdevworks_projects_index = Project::Post.where(post_status: "published").order("post_title ASC")
+      @phcdevworks_projects_index = Project::Post.where(project_post_status: "published").order("project_post_title ASC")
       @phcdevworks_projects_randomizer = Project::Post.order('RANDOM()').limit(1)
     end
 
--- lib/phcdevworks_portfolio/version.rb
+++ lib/phcdevworks_portfolio/version.rb
@@ -1,3 +1,3 @@
 module PhcdevworksPortfolio
-    VERSION = "9.0.0"
+    VERSION = "9.0.1"
 end
