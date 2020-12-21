5b0fdfa2f7d4ebc8a9158b96c953f5ab8e201674
BradPotts <brad.potts@phcnet.co>
2020-12-21 12:22:04 +0000

    upgrade to rails 6.1, adjusted menus for phcaccounts, starting to standardize manifests for testing.

--- app/assets/config/phcdevworks_portfolio_manifest.js
+++ app/assets/config/phcdevworks_portfolio_manifest.js
@@ -1,2 +1,4 @@
 //= link_directory ../stylesheets/phcdevworks_portfolio .scss
 //= link_directory ../stylesheets/phcdevworks_portfolio .js
+//= link phcthemes_admin_panel_pack_coloradmin.css
+//= link phcthemes_admin_panel_pack_coloradmin.js
